f75da4bf7b48293f5ac83bee1f934ad5f066cd80
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-06 23:57:23 -0300

    Add files via upload

--- pacote_tidyr.R
+++ pacote_tidyr.R
@@ -27,4 +27,26 @@ data.frame(iris) # Dados com tabela padrão
 
 ### Construindo um tibble:
 
+# Por colunas
+tibble(x = 1:3, y = c("a", "b", "c"))
 
+# Por linhas: usa-se a função 'tribble'
+tribble(~x, ~y, 
+       1, "a", 
+       2, "b", 
+       3, "c")
+
+# Converte um data.frame em um tibble
+as_tibble(iris)
+
+# Converte vetores nomeados em um tibble
+enframe(x, name = "name", value = "value")
+enframe(1:3)
+enframe(c(a = 5, b = 7))
+enframe(list(one = 1, two = 2:3, three = 4:6))
+deframe(enframe(3:1))
+deframe(tibble(a = 1:3))
+deframe(tibble(a = as.list(1:3)))
+
+# Testa de 'x' é um tibble
+is_tibble(iris)
